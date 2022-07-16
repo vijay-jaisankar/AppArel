0b3cd69c65188e942d0d560c61b137664874dfcd
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2022-07-16 23:48:51 +0530

    add: resized shirts' images, modified resizing script

--- gan/resize_images.sh
+++ gan/resize_images.sh
@@ -1,8 +1,8 @@
-mkdir resized-pants-128-samples
-cd pants-128-samples
+mkdir resized-shirts-64-samples
+cd shirt-64-samples
 
 for img in *.jpg
     do
         echo "Resizing" + ${img};
-        ffmpeg -hwaccel cuda -i ${img} -vf scale=128:128 -preset slow ../resized-pants-128-samples/${img}
+        ffmpeg -hwaccel cuda -i ${img} -vf scale=128:128 ../resized-shirts-64-samples/${img}
     done
